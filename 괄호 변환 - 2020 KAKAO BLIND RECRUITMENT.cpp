404cc6892e93274a077b4eaae8a1b2c319206cf3
LeeSoMyoung <47571973+LeeSoMyoung@users.noreply.github.com>
2021-02-19 20:39:12 +0900

    Create 괄호 변환 - 2020 KAKAO BLIND RECRUITMENT.cpp

---
+++ 괄호 변환 - 2020 KAKAO BLIND RECRUITMENT.cpp
@@ -0,0 +1,47 @@
+
+
+// 문제 링크 : 
+
+#include <bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;++i)
+#define forn(i,n) rep(i,0,n)
+using namespace std;
+
+bool chk(string& str){
+    int len=str.size();
+    stack<char>st;
+    forn(i,len){
+        if(str[i]=='(') st.push('(');
+        else{
+            if(st.empty()) return false;
+            st.pop();
+        }
+    }
+    return true;
+}
+string getAnswer(string str){
+    if(str=="") return "";
+    int left=0, right=0, len=str.size();
+    string lft,rht;
+    forn(i,len){
+        if(str[i]=='(') ++left;
+        else ++right;
+        if(left==right){
+            lft=str.substr(0,i+1);
+            rht=str.substr(i+1);
+            break;
+        }
+    }
+    if(chk(lft)) return lft+getAnswer(rht);
+    else{
+        string tmp="(";
+        tmp+=getAnswer(rht)+")";
+        lft=lft.substr(1,lft.size()-2);
+        len=lft.size();
+        forn(i,len) tmp+=(lft[i]=='('?')':'(');
+        return tmp;
+    }
+}
+string solution(string p) {
+    return chk(p)?p:getAnswer(p);
+}
